9cfc0233a7bb296db194ff5b8b4c3aa53e2851a1
Yi-Shan <43280619+yishanstone@users.noreply.github.com>
2018-10-24 20:51:39 -0400

    Add files via upload

---
+++ Exporter.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import sys,getopt,datetime,codecs
+if sys.version_info[0] < 3:
+    import got
+else:
+    import got3 as got
+
+def main(argv):
+
+	if len(argv) == 0:
+		print('You must pass some parameters. Use \"-h\" to help.')
+		return
+
+	if len(argv) == 1 and argv[0] == '-h':
+		f = open('exporter_help_text.txt', 'r')
+		print f.read()
+		f.close()
+
+		return
+
+	try:
+		opts, args = getopt.getopt(argv, "", ("username=", "near=", "within=", "since=", "until=", "querysearch=", "toptweets", "maxtweets=", "lang=")) #delete "output=" sand add "lang="
+
+		tweetCriteria = got.manager.TweetCriteria()
+		strSince = '' #add
+		strUntil = '' #add
+		strQuery = '' #add
+		strMaxtweet = '' #add
+		strUsername = '' #add
+ 
+		for opt,arg in opts:
+			if opt == '--username':
+				tweetCriteria.username = arg
+                                strUsername = arg #add 
+
+			elif opt == '--since':
+				tweetCriteria.since = arg
+                                strSince = arg #add
+
+			elif opt == '--until':
+				tweetCriteria.until = arg
+                                strUntil = arg #add
+
+			elif opt == '--querysearch':
+				tweetCriteria.querySearch = arg
+                                strQuery = arg #add
+
+			elif opt == '--toptweets':
+				tweetCriteria.topTweets = True
+
+			elif opt == '--maxtweets':
+				tweetCriteria.maxTweets = int(arg)
+                                strMaxtweet = arg #add
+			
+			elif opt == '--near':
+				tweetCriteria.near = '"' + arg + '"'
+			
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+			elif opt == '--within':
+				tweetCriteria.within = '"' + arg + '"'
+
+
+                        elif opt == '--lang':   #add
+                                tweetCriteria.lang = arg   #add
+
+
+		outputFileName = strUsername+'_'+strQuery+'_'+strMaxtweet+'_'+strSince+'_'+strUntil+'.csv'  #add
+
+
+		outputFile = codecs.open(outputFileName, "w+", "utf-8")
+
+
+		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+
+		print('Searching...\n')
+
+		def receiveBuffer(tweets):
+			for t in tweets:
+				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
+			outputFile.flush()
+			print('More %d saved on file...\n' % len(tweets))
+
+		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+
+	except arg:
+		print('Arguments parser error, try -h' + arg)
+	finally:
+		outputFile.close()
+		print('Done. Output file generated ' + outputFileName)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
